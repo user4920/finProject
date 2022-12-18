4b80bacf470d1187ab74ebc155acf2044c7abe03
user4920 <dltjgus@duksung.ac.kr>
2022-12-18 22:50:27 +0900

    댓글 삭제 기능

--- mall/views.py
+++ mall/views.py
@@ -133,6 +133,15 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
     else:
       raise PermissionDenied
 
+def delete_comment(request, pk):
+  comment = get_object_or_404(Comment, pk=pk)
+  post = comment.post
+  if request.user.is_authenticated and request.user == comment.author:
+    comment.delete()
+    return redirect(post.get_absolute_url())
+  else:
+    raise PermissionDenied
+
 class CategoryPage(PostList):
   def get_queryset(self):
     slug = self.kwargs['slug']
