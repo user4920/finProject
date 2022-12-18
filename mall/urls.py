e2d8760287f9297d92e29212d3f7ff5ab382a3d9
user4920 <dltjgus@duksung.ac.kr>
2022-12-18 12:38:15 +0900

    포스트 상세 페이지 작성

--- mall/urls.py
+++ mall/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ # IP주소/mall/
-  path('', views.PostList.as_view())
+  path('', views.PostList.as_view()),
+  path('<int:pk>/', views.PostDetail.as_view()),
 ]
--- mall/views.py
+++ mall/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 class PostList(ListView):
@@ -9,4 +10,11 @@ class PostList(ListView):
 
   def get_context_data(self, *, object_list=None, **kwargs):
     context = super(PostList, self).get_context_data()
+    return context
+
+class PostDetail(DetailView):
+  model = Post
+
+  def get_context_data(self, **kwargs):
+    context = super(PostDetail, self).get_context_data()
     return context
